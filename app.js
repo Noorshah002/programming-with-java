5fb2131f2ce11517b379c49cd0c34fac7e0fa8b7
Noor-Shah <155747087+Noorshah002@users.noreply.github.com>
2024-10-21 19:03:23 +0500

    first class

---
+++ app.js
@@ -0,0 +1,45 @@
+console.log("hello world")
+
+//alert//
+
+window.alert("dont touch the button")
+
+alert("kesa laga mera mazak")
+
+//document write//
+
+document.write(" welcome in our new aptech" + "<br>")
+
+document.write(25+25)
+
+//types of ///
+
+var product="scales";  //string//
+
+var isvalidid =true;   //boolen//
+
+var year = 2025;     //number//
+
+
+console.log(typeof(product))    
+console.log(typeof(isvalidid ))   
+console.log(typeof(year))
+
+
+
+var firstname; // initailize
+var firstname="Noor Shah";//DECLARE//
+
+
+console.log(firstname)
+
+
+
+//DATA TYPES 6 PRIMITIVE//
+
+//  01 . string //
+//  02 . boolen//
+//  03 . number//
+//  04 . null//
+//  05 . undefined//
+//  06 . symbol//

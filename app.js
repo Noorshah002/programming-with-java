aede4c1fa169a58a24dccca3d2bc8ab5cee8d9a8
Noor-Shah <155747087+Noorshah002@users.noreply.github.com>
2024-10-23 19:05:27 +0500

    Update app.js

--- app.js
+++ app.js
@@ -43,3 +43,170 @@ console.log(firstname)
 //  04 . null//
 //  05 . undefined//
 //  06 . symbol//
+
+
+///complex data type 
+///array 
+//object
+//function
+
+var fname="noor-shah"
+console.log(fname);
+
+
+fname="syeda-noor-shah"
+console.log(fname)
+
+
+fname="noor"
+console.log(fname)
+
+//var in number
+
+var marks = 100;
+var weight = 60.5;
+var salary = 4000;
+
+console.log(marks)
+
+//var in boolen 
+
+var isvalid = true;
+var isnot = false;
+
+console.log(isvalid)
+
+
+//statments
+
+var a = 20;  //statment
+var b = "open";  //statment
+var c = false;  //statment
+var d = 50;  //statment
+
+console.log(c)
+
+
+//statment end with semi-colon (;)
+
+
+  i="syeda"; j="noor"; k="shah";
+
+  console.log(i)
+  console.log(j)
+  console.log(k)
+
+  //new line will break the statement
+
+  var a = 20 
+  var b = a*10
+
+  console.log(a)
+  console.log(b)
+
+  //expression
+
+  2+3;  //expression
+  4/4;    //expression
+  var a = 40;   //statment
+  a*4;    //expression
+  "syeda"+"noor"+"shah"   //expression
+  
+  
+//multiline comment and single line comment
+
+/* 
+a = 3;
+a = 3;
+a = 3;
+*/
+
+console.log(a)
+
+
+// Variable legal or illegal names
+// legal names
+
+// var hello = 'goodmorning';
+// var _abc = 23;
+// var $work = true;
+// var user2 = 'Owais';
+// var my$team = 45;
+// var car_registered = 'abc-456'
+// var isPassed = false;
+
+// // illegal names
+// var 3user = 'abc'; 
+// var my-fname = 'owais';
+// var my name = 'abc';
+// var car#register = 'sedan';
+// var user&isavaible = 'abc';
+// var user*isavaible = 'abc';
+// var user?isavaible = 'abc';
+
+
+
+
+
+
+
+
+//case sensitive
+
+var tulip = "pink"
+var Tulip = " red"
+var TULIP = "white"
+
+console.log(tulip)
+console.log(Tulip)
+console.log(TULIP)
+
+
+//camel  case 
+var responsivetime;
+var timeline;
+var userresonsetimelimit;
+
+
+
+//arthematic operator
+var a = +prompt("Enter first number")
+var b = +prompt("Enter second number")
+
+var add = a+b
+console.log(  " add result is " +add)
+
+
+
+//subtract
+
+
+var subtract = a-b
+console.log(   " subtract result is " +subtract)
+
+//multiply
+
+
+var multiply = a*b
+console.log(   " multiply result is " +multiply)
+
+
+//divide
+
+
+var divide = a/b
+console.log( " divide result is " +divide)
+
+//power
+
+
+var power = a**b
+console.log(  " power result is " +power)
+
+
+var reminder = a%b
+console.log(  " reminder result is " +reminder)
+
+
+//EXAMPLE                  //SAME AS 
+
